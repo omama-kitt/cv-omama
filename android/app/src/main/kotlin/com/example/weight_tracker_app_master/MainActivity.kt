8f3ae77579fc7d5034b371ec9278ef13e4b989b9
omama-kitt <omama.kitt2000@gmail.com>
2023-01-15 20:17:24 +0200

    firebase

---
+++ android/app/src/main/kotlin/com/example/weight_tracker_app_master/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weight_tracker_app_master
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/widgets/custom_bottom_nav_bar.dart
@@ -0,0 +1,33 @@
+/*import 'package:flutter/material.dart';
+import 'package:weight_tracker/view/home_screen_view_model.dart';
+
+Widget CustomBottomNavBar(HomeScreenViewModel model) {
+  return BottomNavigationBar(
+    currentIndex: model.selectedIndex,
+    selectedItemColor: Colors.black,
+    onTap: model.onItemTapped,
+    items: <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+        backgroundColor: Color.fromARGB(255, 165, 133, 170),
+      ),
+      // BottomNavigationBarItem(
+      //   title: Text('Statistics'),
+      //   icon: Icon(Icons.auto_graph_rounded),
+      //   backgroundColor: Colors.lightBlue,
+      // ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.history),
+        label: 'Home',
+        backgroundColor: Color.fromARGB(255, 165, 133, 170),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.person_rounded),
+        label: 'Home',
+        backgroundColor: Color.fromARGB(255, 165, 133, 170),
+      ),
+    ],
+  );
+}
+*/
